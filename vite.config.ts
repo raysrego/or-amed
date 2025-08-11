fc66f802c4e1a40637783a714ed112698482c455
raysrego <rayannyrego@gmail.com>
2025-08-11 16:16:55 -0300

    Corrigir erro de importação do React

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   root: '.',
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['react', 'react-dom'],
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
 });
